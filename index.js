7f70b95cf94652747767a855eb8bea136e139fe4
Nikolay <freshitftm@gmail.com>
2021-05-20 10:10:14 +0300

    Add react class component

--- index.js
+++ index.js
@@ -5,11 +5,20 @@ ReactDOM
 
 const container = document.getElementById("root");
 
-const reactElement = React.createElement("h1", {
-  title: "Hello react!",
-  className: "heading",
-}, "Hello React.js!");
+class Heading extends React.Component {
+  render() {
+    return React.createElement(
+      "h1",
+      {
+        title: "Hello react!",
+        className: "heading",
+      },
+      "Hello React.js!"
+    );
+  }
+}
 
-console.log(reactElement);
+const reactElement = React.createElement(Heading);
 
 ReactDOM.render(reactElement, container);
+
